8e23138e25c68cdb65889299edebf395fe4f1e31
David Arias <david-0296@hotmail.com>
2018-07-25 03:46:21 +0000

    MaterialUI and Grid

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { Grid, Row, Col } from 'react-flexbox-grid';
+import Paper from '@material-ui/core/Paper';
+import AppBar from '@material-ui/core/AppBar';
+import Typography from '@material-ui/core/Typography';
 import LocationList from './components/LocationList';
 
 import './App.css';
@@ -22,22 +25,23 @@ class App extends Component {
   render() {
     return (
       <Grid>
+        <AppBar position="static">
+          <Typography variant="title" color="inherit">
+            Weather App
+          </Typography>
+        </AppBar>
         <Row>
-          <Col xs={12} sm={6} md={4}>
-            <div className='red'>MD</div>
+          <Col xs={12} md={6}>
+            <LocationList cities={ cities } 
+              onSelectedLocation= { this.handleSelectedLocation }/>
           </Col>
-          <Col xs={12} sm={6} md={4}>
-            <div className='green'>MD</div>
-          </Col>
-          <Col xs={12} sm={6} md={4}>
-            <div className='blue'>MD</div>
+          <Col xs={12} md={6}>
+            <Paper zDepth={4}>
+              <div className='detail'></div>
+            </Paper>
           </Col>
         </Row>
       </Grid>
-      /*<div className="App">
-        <LocationList cities={ cities } 
-          onSelectedLocation= { this.handleSelectedLocation }/>
-      </div>*/
     );
   }
 }
